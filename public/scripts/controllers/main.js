953caa783b6c0da5b1890f38a2fd2b5a533cca0b
Michael England <michael.england@me.com>
2013-11-18 20:14:50 -0800

    Updated home.

--- public/scripts/controllers/main.js
+++ public/scripts/controllers/main.js
@@ -1,10 +1,4 @@
 'use strict';
 
 angular.module('mongoConductorApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('MainCtrl', function($scope) {});
