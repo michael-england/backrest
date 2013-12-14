4a0cdb665d4dd28f6d42ca6ec56c55bb96a3c2dc
Michael England <michael.england@me.com>
2013-12-14 10:43:56 -0800

    Added status codes to errors. Cleaned up code.

--- server.js
+++ server.js
@@ -1,215 +1,209 @@
-'use strict';
+(function() {
+  'use strict';
 
-var http = require('http');
-var https = require('https');
-var events = require('events');
-var fs = require('./lib/fs');
-var url = require('url');
-var util = require('util');
-var email = require('./node_modules/emailjs/email');
-var express = require('express');
-var mongoose = require('mongoose');
-var MongoStore = require('connect-mongo')(express);
+  var http = require('http');
+  var https = require('https');
+  var events = require('events');
+  var fs = require('./lib/fs');
+  var url = require('url');
+  var util = require('util');
+  var email = require('./node_modules/emailjs/email');
+  var express = require('express');
+  var mongoose = require('mongoose');
+  var MongoStore = require('connect-mongo')(express);
 
-var Backrest = function() {
-  events.EventEmitter.call(this);
-  this.uploads = require('./lib/uploads');
-  this.app = express();
+  var Backrest = function() {
+    events.EventEmitter.call(this);
+    this.uploads = require('./lib/uploads');
+    this.app = express();
 
-  this.init = function() {
-    var settingsFilename = './settings.json';
-    if (fs.existsSync(settingsFilename)) {
+    this.init = function() {
+      var settingsFilename = './settings.json';
+      if (fs.existsSync(settingsFilename)) {
 
-      // read the settings file
-      var file = fs.readFileSync(settingsFilename, {
-        'encoding': 'binary'
-      });
+        // read the settings file
+        var file = fs.readFileSync(settingsFilename, {
+          'encoding': 'binary'
+        });
 
-      // parse file to this.settings object
-      this.settings = JSON.parse(file);
+        // parse file to this.settings object
+        this.settings = JSON.parse(file);
 
-      // init mongoose
-      this.mongoose = mongoose;
-      this.mongoose.connect(this.settings.databaseUrl);
-      this.mongoose.set('cache', false);
+        // init mongoose
+        this.mongoose = mongoose;
+        this.mongoose.connect(this.settings.databaseUrl);
+        this.mongoose.set('cache', false);
 
-      // perform initial setup
-      if (this.settings.setup && !fs.existsSync('./INSTALLED')) {
-        require('./lib/setup')(this, function() {
+        // perform initial setup
+        if (this.settings.setup && !fs.existsSync('./INSTALLED')) {
+          require('./lib/setup')(this, function() {
+
+            // start the servers
+            this.smtpStart();
+            this.httpStart();
+          }.bind(this));
+        } else {
 
           // start the servers
           this.smtpStart();
           this.httpStart();
-        }.bind(this));
+        }
       } else {
 
-        // start the servers
-        this.smtpStart();
-        this.httpStart();
+        // handle error
+        console.log('settings.json file does not exist.');
+
+        // exit the application
+        process.exit();
       }
-    } else {
+    };
 
-      // handle error
-      console.log('settings.json file does not exist.');
+    this.loadMail = function(key) {
 
-      // exit the application
-      process.exit();
-    }
-  };
-
-  this.loadMail = function(key) {
-
-    // text version
-    var message = this.settings.mail.messages[key];
-    if (fs.existsSync(message.text)) {
-      message.text = fs.readFileSync(message.text, {
-        'encoding': 'binary'
-      });
-    }
-
-    // html version
-    var attachment = message.attachment;
-    if (attachment && attachment[0] && attachment[0].alternative === true) {
-      if (fs.existsSync(attachment[0].data)) {
-        attachment[0].data = fs.readFileSync(attachment[0].data, {
+      // text version
+      var message = this.settings.mail.messages[key];
+      if (fs.existsSync(message.text)) {
+        message.text = fs.readFileSync(message.text, {
           'encoding': 'binary'
         });
       }
-    }
-  };
 
-  this.smtpStart = function() {
-
-    // create mail server
-    if (this.settings.mail) {
-      this.mail = email.server.connect(this.settings.mail.server);
-      this.loadMail('confirmEmail');
-      this.loadMail('passwordResetRequest');
-      this.loadMail('errorEmail');
-    }
-  }
-
-  this.httpStart = function(callback) {
-
-    // define the session
-    var session = {};
-    if (this.settings.session) {
-      session = JSON.parse(JSON.stringify(this.settings.session));
-      if (session.store) {
-
-        if (!session.store.url) {
-          session.store.url = this.settings.databaseUrl;
+      // html version
+      var attachment = message.attachment;
+      if (attachment && attachment[0] && attachment[0].alternative === true) {
+        if (fs.existsSync(attachment[0].data)) {
+          attachment[0].data = fs.readFileSync(attachment[0].data, {
+            'encoding': 'binary'
+          });
         }
+      }
+    };
 
-        session.store = new MongoStore(session.store);
+    this.smtpStart = function() {
+
+      // create mail server
+      if (this.settings.mail) {
+        this.mail = email.server.connect(this.settings.mail.server);
+        this.loadMail('confirmEmail');
+        this.loadMail('passwordResetRequest');
+        this.loadMail('errorEmail');
       }
     }
 
-    // initialize the session state
-    this.app.use(express.bodyParser());
-    this.app.use(express.cookieParser());
-    this.app.use(express.session(session));
-    this.app.use('/', express.static('./public'));
-    this.app.use('/uploads', './uploads');
-    this.app.use(require('./lib/error')(this));
-    this.app.use(require('./lib/render')(this));
-    this.app.use(require('./lib/users')(this));
-    this.app.use(require('./lib/uploads')(this));
-    this.app.use(require('./lib/collections')(this));
+    this.httpStart = function(callback) {
 
-    if (this.settings.https) {
-      if (this.settings.https.enabled) {
-        if (this.settings.https.privateKey !== undefined && this.settings.https.privateKey !== '' && this.settings.https.certificate !== undefined && this.settings.https.certificate !== '') {
+      // define the session
+      var session = {};
+      if (this.settings.session) {
+        session = JSON.parse(JSON.stringify(this.settings.session));
+        if (session.store) {
 
-          var options = {
-            key: fs.readFileSync(this.settings.https.privateKey).toString(),
-            cert: fs.readFileSync(this.settings.https.certificate).toString()
-          };
+          if (!session.store.url) {
+            session.store.url = this.settings.databaseUrl;
+          }
 
-          https.createServer(options, this.app).listen(this.settings.https.port || 443, function() {
-            console.log('HTTPS Server running on port ' + (this.settings.https.port || 443) + '.');
+          session.store = new MongoStore(session.store);
+        }
+      }
 
-            if (this.settings.http) {
-              if (this.settings.http.enabled) {
-                http.createServer(this.app).listen(this.settings.http.port || 80, undefined, function() {
-                  console.log('HTTP Server running on port ' + (this.settings.http.port || 80) + '.');
-                }.bind(this));
-              }
-            }
+      // initialize the session state
+      this.app.use(express.bodyParser());
+      this.app.use(express.cookieParser());
+      this.app.use(express.session(session));
+      this.app.use('/', express.static('./public'));
+      this.app.use('/uploads', './uploads');
+      this.app.use(require('./lib/error')(this));
+      this.app.use(require('./lib/render')(this));
+      this.app.use(require('./lib/users')(this));
+      this.app.use(require('./lib/uploads')(this));
+      this.app.use(require('./lib/collections')(this));
+
+      if (this.settings.https) {
+        if (this.settings.https.enabled) {
+          if (this.settings.https.privateKey !== undefined && this.settings.https.privateKey !== '' && this.settings.https.certificate !== undefined && this.settings.https.certificate !== '') {
+
+            var options = {
+              key: fs.readFileSync(this.settings.https.privateKey).toString(),
+              cert: fs.readFileSync(this.settings.https.certificate).toString()
+            };
+
+            https.createServer(options, this.app).listen(this.settings.https.port || 443, function() {
+              console.log('HTTPS Server running on port ' + (this.settings.https.port || 443) + '.');
+            }.bind(this));
+          } else {
+            throw new Error('HTTPS credientials are not valid.');
+          }
+        }
+      }
+
+      if (this.settings.http) {
+        if (this.settings.http.enabled) {
+          http.createServer(this.app).listen(this.settings.http.port || 80, undefined, function() {
+            console.log('HTTP Server running on port ' + (this.settings.http.port || 80) + '.');
           }.bind(this));
-        } else {
-          throw new Error('HTTPS credientials are not valid.');
         }
       }
-    }
+    };
 
-    if (this.settings.http) {
-      if (this.settings.http.enabled) {
-        http.createServer(this.app).listen(this.settings.http.port || 80, undefined, function() {
-          console.log('HTTP Server running on port ' + (this.settings.http.port || 80) + '.');
-        }.bind(this));
-      }
-    }
-  };
+    this.result = function(request, response, result) {
+      if (response) {
+        var uri = url.parse(request.url, true);
+        if (uri.query.callback) {
 
-  this.result = function(request, response, result) {
-    if (response) {
-      var uri = url.parse(request.url, true);
-      if (uri.query.callback) {
+          // jsonp response
+          response.writeHead(200, {
+            'Content-Type': 'text/javascript',
+            'Access-Control-Allow-Origin': '*'
+          });
+          response.end(uri.query.callback + '(' + JSON.stringify(result) + ')');
+        } else {
 
-        // jsonp response
-        response.writeHead(200, {
-          'Content-Type': 'text/javascript',
-          'Access-Control-Allow-Origin': '*'
-        });
-        response.end(uri.query.callback + '(' + JSON.stringify(result) + ')');
+          // json response
+          response.writeHead(200, {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+          });
+          response.end(JSON.stringify(result));
+        }
       } else {
-
-        // json response
-        response.writeHead(200, {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*'
-        });
-        response.end(JSON.stringify(result));
+        return result;
       }
-    } else {
-      return result;
-    }
-  };
+    };
 
-  this.error = function(request, response, message, statusCode) {
-    if (response) {
+    this.error = function(request, response, message, statusCode) {
+      if (response) {
 
-      // Internal error occurred, create internal error response
-      var json = {
-        'error': message
-      };
+        // Internal error occurred, create internal error response
+        var json = {
+          'error': message
+        };
 
-      var query = url.parse(request.url, true).query;
-      if (query.callback) {
+        var query = url.parse(request.url, true).query;
+        if (query.callback) {
 
-        // jsonp response
-        response.writeHead(200, {
-          'Content-Type': 'text/javascript',
-          'Access-Control-Allow-Origin': '*'
-        });
-        response.end(query.callback + '(' + JSON.stringify(json) + ')');
+          // jsonp response
+          response.writeHead(200, {
+            'Content-Type': 'text/javascript',
+            'Access-Control-Allow-Origin': '*'
+          });
+          response.end(query.callback + '(' + JSON.stringify(json) + ')');
+        } else {
+
+          // json response
+          response.writeHead(statusCode || 200, {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+          });
+          response.end(JSON.stringify(json));
+        }
       } else {
-
-        // json response
-        response.writeHead(statusCode || 200, {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*'
-        });
-        response.end(JSON.stringify(json));
+        return message;
       }
-    } else {
-      return message;
-    }
+    };
   };
-};
 
-util.inherits(Backrest, events.EventEmitter);
+  util.inherits(Backrest, events.EventEmitter);
 
-var backrest = new Backrest();
-backrest.init();
+  var backrest = new Backrest();
+  backrest.init();
+})();
